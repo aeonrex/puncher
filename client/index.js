e4373eb002a3f15d96bee92186ce895101bb2e5f
Taylor Kisor-Smith <tfkisorsmith@gmail.com>
2015-10-14 21:51:13 -0700

    Let's try udp.

--- client/index.js
+++ client/index.js
@@ -4,32 +4,83 @@ var net = require('net');
 var http = require('./http');
 var host = process.env.HOST || '127.0.0.1';
 var port = process.env.PORT || 13337;
+var clientName = process.env.CLIENT_NAME || 'client1';
 var localAddress = '0.0.0.0' || '127.0.0.1';
 
-var client = new net.Socket();
+// var client = new net.Socket();
+//
+// client.setKeepAlive(true);
+//
+// client.on('data', function (data) {
+//   console.log(data.toString());
+// });
+//
+//
+// client.connect({
+//   port: port,
+//   host: host,
+// //  localAddress: localAddress, // 127.0.0.1 for mac, linux likes 0.0.0.0 - need to try 'localhost'
+//
+// }, function () {
+//   console.log('Connected to server.');
+//   console.log(client.address());
+//   var address = client.address();
+//
+//   http.bootstrap(address, function connected(err) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log('aw yiss');
+//     client.write('Come on and connect');
+//   });
+// });
+var udp = require('dgram');
+var client = udp.createSocket('udp4');
 
-client.setKeepAlive(true);
+var getNetworkIP = function (callback) {
+  var socket = net.createConnection(80, 'www.google.com');
+  socket.on('connect', function() {
+    callback(undefined, socket.address().address);
+      socket.end();
+  });
+  socket.on('error', function(e) {
+    callback(e, 'error');
+  });
+};
 
-client.on('data', function (data) {
-  console.log(data.toString());
-});
+var send = function (connection, msg, cb) {
+  var data = new Buffer(JSON.stringify(msg));
 
-
-client.connect({
-  port: port,
-  host: host,
-//  localAddress: localAddress, // 127.0.0.1 for mac, linux likes 0.0.0.0 - need to try 'localhost'
-
-}, function () {
-  console.log('Connected to server.');
-  console.log(client.address());
-  var address = client.address();
-
-  http.bootstrap(address, function connected(err) {
+  client.send(data, 0, data.length, connection.port, connection.address,
+    function(err, bytes) {
     if (err) {
-      return console.log(err);
+      client.close();
+      console.log('# stopped due to error: %s', err);
+    } else {
+      console.log('# sent %s to %s:%s', msg.type, connection.address, connection.port);
+      if (cb) cb();
     }
-    console.log('aw yiss');
-    client.write('Come on and connect');
+  });
+};
+
+client.on('listening', function () {
+  var linfo = { port: client.address().port };
+  getNetworkIP(function(error, ip) {
+    if (error) {
+      console.log(error);
+      return console.log("! Unable to obtain connection information!");
+    }
+    linfo.address = ip;
+    console.log('# listening as %s@%s:%s', clientName, linfo.address, linfo.port);
+    send({address: host, port: port}, {type: 'register', name: clientName, linfo: linfo});
+    setInterval(function () {
+      send({address: host, port: port}, {type: 'register', name: clientName, linfo: linfo});
+    }, 20000);
   });
 });
+
+client.on('message', function (data, rinfo) {
+  console.log('Message from %s:%s', rinfo.address, rinfo.port);
+});
+
+client.bind();
--- server/server.js
+++ server/server.js
@@ -4,30 +4,68 @@ var net = require('net');
 var log = require('util').log;
 var port = process.env.PORT || 13337;
 
-var server = net.createServer(function (sock) {
-  //let address = conn.address();
-  log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+// var server = net.createServer(function (sock) {
+//   //let address = conn.address();
+//   log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+//
+//   log(sock.address());
+//   log(server.address());
+//
+//   sock.write(sock.remoteAddress + ':' + sock.remotePort);
+//
+//
+//   sock.on('close', function () {
+//     log('Disconnecting from ' + sock.remoteAddress + ':' + sock.remotePort);
+//   });
+//
+//   sock.on('data', function (data) {
+//     log(sock.remoteAddress + ':' + sock.remotePort + ' says: ' + data.toString());
+//   });
+//
+//   sock.on('error', function (err) {
+//     log('An error occurred: ' + err);
+//   });
+//
+// });
+//
+// log(port);
+//
+// server.listen(port, '0.0.0.0');
 
-  log(sock.address());
-  log(server.address());
 
-  sock.write(sock.remoteAddress + ':' + sock.remotePort);
+var udp = require('dgram');
+var server = udp.createSocket('udp4');
 
-
-  sock.on('close', function () {
-    log('Disconnecting from ' + sock.remoteAddress + ':' + sock.remotePort);
-  });
-
-  sock.on('data', function (data) {
-    log(sock.remoteAddress + ':' + sock.remotePort + ' says: ' + data.toString());
-  });
-
-  sock.on('error', function (err) {
-    log('An error occurred: ' + err);
-  });
-
+server.on('listening', function () {
+  var address = server.address();
+  console.log('# listening [%s:%s]', address.address, address.port);
 });
 
-log(port);
+server.on('message', function (data, rinfo) {
+  try {
+   data = JSON.parse(data);
+ } catch (e) {
+   return console.log('! Couldn\'t parse data (%s):\n%s', e, data);
+ }
+  console.log('# Client registered: %s@[%s:%s | %s:%s]', data.name,
+                rinfo.address, rinfo.port, data.linfo.address, data.linfo.port);
+  send(rinfo.address, rinfo.port, {
+    client: rinfo.address,
+    port: rinfo.port
+  });
+});
 
-server.listen(port, '0.0.0.0');
+var send = function(host, port, msg, cb) {
+  var data = new Buffer(JSON.stringify(msg));
+  server.send(data, 0, data.length, port, host, function(err, bytes) {
+    if (err) {
+      server.close();
+      console.log('# stopped due to error: %s', err);
+    } else {
+      console.log('# sent '+msg.client);
+      if (cb) cb();
+    }
+  });
+};
+
+server.bind(port);
